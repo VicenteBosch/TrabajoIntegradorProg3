fb711fac5b4e3ebebcb5a5a94fb0c6b15dc29617
Tomás <tomiricci@MacBook-Pro-de-Tomi-2.local>
2024-09-23 11:21:57 -0300

    Favoritos

--- src/screens/Favoritos.js
+++ src/screens/Favoritos.js
@@ -6,14 +6,34 @@ class Favoritos extends Component {
     constructor (props){
         super(props);
         this.state = { 
+            peliculasFavoritas: []
+        }
+    }
 
+    componentDidMount(){
+        let storage = localStorage.getItem("categoriasFavoritos")
+        if(storage !== null){
+            let storageParseado = JSON.parse(storage)
+
+            Promise.all(
+                storageParseado.map(elm => 
+                    fetch(`https://api.themoviedb.org/3/movie/${elm}?api_key=0e2ea5762304016279ec676c08bd2b6d`)
+                    .then(res => res.json())
+                )
+            )
+            .then(data => this.setState({peliculasFavoritas: data}))
+            
         }
     }
     
     render(){
         return(
             <div>
-         <h2>Favoritos</h2>
+            {
+                this.state.peliculasFavoritas.length > 0 ?
+                    this.state.peliculasFavoritas.map(elm => <h1>{elm.title}</h1>)
+                    : <h1>No hay peliculas Favoritas</h1>
+            }
             </div>
         )
     }
